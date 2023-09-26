414e267fba3a59740733ab070071d4b991e7e1b0
alanbrantley <67926592+alanbrantley@users.noreply.github.com>
2023-09-25 19:16:23 -0700

    removed skills section

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,26 @@
-import Head from 'next/head'
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Main from '../components/Main'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
-
+import Head from "next/head";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import Main from "../components/Main";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Alan | Designer and Developer</title>
-        <meta name="description" content="I’m a designer and developer specializing in prompt engineering and accessible design" />
+        <meta
+          name="description"
+          content="I’m a designer and developer specializing in prompt engineering and accessible design"
+        />
         <link rel="icon" href="/fav.png" />
       </Head>
-    <Main />
-    <About />
-    <Skills />
-    <Projects />
-    <Contact />
+      <Main />
+      <About />
+      {/* <Skills /> */}
+      <Projects />
+      <Contact />
     </div>
-  )
+  );
 }
