b87931ee6e29cbb26640733385b5491d3c4e4850
alanbrantley <67926592+alanbrantley@users.noreply.github.com>
2024-05-18 23:18:45 -0700

    restore ab.com test

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
-import Chapters from "../components/Chapters";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 
 export default function Home() {
@@ -20,7 +20,7 @@ export default function Home() {
       <About />
       {/* <Skills /> */}
       <Projects />
-      <Contact />
+      {/* <Contact /> */}
     </div>
   );
 }
