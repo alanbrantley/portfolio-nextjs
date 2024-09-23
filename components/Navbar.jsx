b9b3e43ba76519b94eba8d2f94fdd0debc2ff182
alanbrantley <67926592+alanbrantley@users.noreply.github.com>
2024-09-22 18:32:33 -0700

    Updated mobile hamburger menu
    removed "home", "skills", "resume", and added "writing"

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -112,19 +112,9 @@ const Navbar = () => {
           </div>
           <div className="py-4 flex flex-col">
             <ul className="uppercase">
-              <Link href="/">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                  Home
-                </li>
-              </Link>
               <Link href="/#about">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
                   About
-                </li>
-              </Link>
-              <Link href="/#skills">
-                <li onClick={() => setNav(false)} className="py-4 text-sm">
-                  Skills
                 </li>
               </Link>
               <Link href="/#projects">
@@ -132,11 +122,11 @@ const Navbar = () => {
                   Projects
                 </li>
               </Link>
-              {/* <Link href="/resume">
+              <Link href="/writing">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
-                  Resume
+                  Writing
                 </li>
-              </Link> */}
+              </Link>
               <Link href="/#contact">
                 <li onClick={() => setNav(false)} className="py-4 text-sm">
                   Contact
